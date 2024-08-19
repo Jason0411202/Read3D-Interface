cc79abd9dda167145a5023aeb86f190c76dcc1f5
Jason0411202 <Jason0411202@gmail.com>
2024-08-19 23:25:12 +0800

    edit sh

--- script/runReal3D.sh
+++ script/runReal3D.sh
@@ -1,5 +1,5 @@
-source /mnt/Nami/users/Jason0411202/anaconda3/bin/activate
-conda activate real3dportrait
+source /mnt/Nami/users/Jason0411202/anaconda3/bin/activate /mnt/Nami/users/Jason0411202/anaconda3/envs/real3dportrait
+echo "Current Conda environment: $(conda info --envs | grep '*' | awk '{print $1}')"
 
 # 取得傳入的參數
 param1=$1
@@ -11,4 +11,10 @@ echo "Parameter 1: $param1"
 echo "Parameter 2: $param2"
 echo "Parameter 3: $param3"
 
-python ../Real3DPortrait/inference/real3d_infer.py --src_img "$param1" --drv_aud "$param2" --drv_pose "default_drvpose.mp4" --out_name "$param3" --out_mode "final"
+cd ../Real3DPortrait
+
+python_command="python ../Real3DPortrait/inference/real3d_infer.py --src_img \"$param1\" --drv_aud \"$param2\" --drv_pose \"../default_drvpose.mp4\" --out_name \"$param3\" --out_mode \"final\""
+echo "Executing command: $python_command"
+
+# 執行 python 指令
+eval $python_command
